3ade490a72cbf3d7f83f35c9ea0e261a1c19554d
Sam Portelance <sportelance1@gmail.com>
2021-01-15 20:36:56 -0500

    Boilerplate complete. End of day 1

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="entry"></div>
+    );
+  }
+}
