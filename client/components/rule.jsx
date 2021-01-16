6d1c5cad487e43fd76215f8520b16f6c8e54856c
Sam Portelance <sportelance1@gmail.com>
2021-01-16 15:18:19 -0500

    set state works, site renders, controllers added in config, state moved to seperate files

--- client/components/rule.jsx
+++ client/components/rule.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Rule from './rule.jsx'
 
-class Rules extends Component{
+class Rule extends Component{
   constructor(props){
     super(props);
   }
 
   render(){
     return(
-      <div class="rule" onclick={changeRuleStatus}>
+      <div class="rule">
         <h3>NAME</h3>
       </div>
     )
   }
 }
 
-export default Rules;
+export default Rule;
