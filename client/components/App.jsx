c3e5ee2def55196be2b4629d9938e30b7d4336dc
Richard <richard@Richards-iMac.local>
2021-01-16 12:46:00 -0500

    finished signin page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import SignIn from './SignIn.jsx';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
     return (
-      <div id="entry"></div>
+      <div id="entry">
+        <div id="title">EZ-Linter</div>
+        <SignIn />
+      </div>
     );
   }
 }
