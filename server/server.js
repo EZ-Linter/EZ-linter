0fb8521a77f973423dfa024bd6935e1ce84ebc38
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-15 20:44:32 -0500

    setup basic routes to get and save configurations

--- server/server.js
+++ server/server.js
@@ -3,13 +3,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+
 if(process.env.NODE_ENV === 'production'){
   app.use('/build', express.static(path.join(__dirname, '../build')));
+
   app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/index.html'));
   });
 }
+
+// get user saved configs
+app.get('/api/:user', (req, res) => {})
+
+// retrieve configuration from database
+app.get('/api/config/:id', (req, res) => {})
+
+// save configuration to database
+app.post('/api/config', (req, res) => {})
+
+// oAuth callback route
+app.use('/api/user/signin/callback', (req, res) => {})
+
 app.listen(PORT, () => {
   console.log(`it's going down at: ${PORT}`);
 });
+
 module.exports = app;
