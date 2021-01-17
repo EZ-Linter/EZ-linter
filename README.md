ffef3ddd4afc6b479c59fbaca86604cd81da4819
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-16 19:53:20 -0500

    add and remove user configurations

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Settings
 
-1. Copy server/settings.example.js to server/settings.js and change values to match your MongoDB and Github OAuth settings. 
+1. Copy server/settings.example.js to server/settings.js and change values to match your MongoDB and Github OAuth settings.
 
 ### Recommended database setup
 
@@ -11,6 +11,7 @@
 
 ## Endpoints
 
+- `POST /api/user/config`: add config to database if not already present and adds configId to list of user's saved configs. Return configId in body of response as {configId: '<configId>'}
+- `DELETE /api/user/config`: remove config from user list of saved configs
+- `GET /api/user/savedconfigs`: get user's list of saved config ids. Returned in body of response as {configs: configs[]}
 - `GET /api/config/:id`: returns eslintrc config from database. If id is not found, returns status 404.
-- `POST /api/config`: returns id of config on database. Inserts new document in database if doesn't already exist. 
-=======
---
+++ server/controllers/userControllers.js
@@ -0,0 +1,62 @@
+const { User } = require('../models/models.js');
+
+const userControllers = {};
+
+/**
+ * Gets list of configs saved by user or empty array and stores in res.locals.userConfigs
+ *
+ * Must have userId on res.locals.userId
+ */
+userControllers.getConfigs = (req, res, next) => {
+  if (!res.locals.userId)
+    return next({ log: 'userControllers.getUserConfigs did not receive an userId' });
+
+  User.findOne({ userId: res.locals.userId }, null, { lean: true })
+    .then((user) => {
+      res.locals.userConfigs = user.configs || [];
+      next();
+    })
+    .catch((err) => next({ log: 'userControllers.getUserConfigs: Failed to find user document' }));
+};
+
+/**
+ * Adds configuration Id to user saved Configs
+ *
+ * Must have configId on res.locals.configId and userId on res.locals.userId
+ */
+userControllers.addConfig = (req, res, next) => {
+  if (!res.locals.userId)
+    return next({ log: 'userControllers.getUserConfigs did not receive an userId' });
+  if (!res.locals.configId)
+    return next({ log: 'userControllers.addConfig did not receive an userId' });
+
+  User.findOneAndUpdate(
+    { userId: res.locals.userId },
+    { $addToSet: { configs: res.locals.configId } },
+    { upsert: true, lean: true }
+  )
+    .then((userDoc) => next())
+    .catch((err) => next({ log: 'userControllers.addConfig failed to update document' + err }));
+};
+
+/**
+ * Removes configuration Id from user saved Configs
+ *
+ * Must have configId on req.body.configId and userId on res.locals.userId
+ */
+userControllers.removeConfig = (req, res, next) => {
+  if (!res.locals.userId)
+    return next({ log: 'userControllers.getUserConfigs did not receive an userId' });
+  if (!req.body.configId)
+    return next({ log: 'userControllers.addConfig did not receive an userId' });
+
+  User.findOneAndUpdate(
+    { userId: res.locals.userId },
+    { $pull: { configs: req.body.configId } },
+    { lean: true }
+  )
+    .then((userDoc) => next())
+    .catch((err) => next({ log: 'userControllers.removeConfig failed to update document' + err }));
+};
+
+module.exports = userControllers;
