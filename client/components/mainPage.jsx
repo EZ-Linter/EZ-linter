2ab72955fa746e962e34d9432b43d4ae2f339cce
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-17 10:24:38 -0500

    implement Export button

--- client/components/mainPage.jsx
+++ client/components/mainPage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Config from './config.jsx'
+import Config from './config.jsx';
 // import * as Actions from './actions/actions.js'
+import ExportBtn from './ExportBtn.jsx';
 
 class Main extends Component {
   constructor(props) {
@@ -11,13 +12,18 @@ class Main extends Component {
   render() {
     return (
       <div id="main">
+        {/* <ExportBtn config={/* should receive config object from state once it's pulled up */} /> */}
         <Config />
       </div>
     );
   }
 }
 
-{/* <form method="GET" action='/createroom'> */}
-{/* <form method="POST" action='/joinroom'></form> */}
+{
+  /* <form method="GET" action='/createroom'> */
+}
+{
+  /* <form method="POST" action='/joinroom'></form> */
+}
 
 export default Main;
