b86e600d3c33b7110b862ae9ae8c0c18cfb8fdcd
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-18 17:52:39 -0500

    write link to clipboard

--- client/components/ShareBtn.jsx
+++ client/components/ShareBtn.jsx
@@ -5,12 +5,21 @@ export default function ShareBtn({ config }) {
     fetch('/api/config/share', {
       method: 'POST',
       headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-      body: JSON.stringify({eslintrc:config}),
+      body: JSON.stringify({ eslintrc: config }),
     })
       .then((res) => res.json())
       .then(({ endpoint }) => {
-        const shareUrl = `${window.location}${endpoint}`
-        window.alert(shareUrl)
+        const shareUrl = `${window.location}${endpoint}`;
+
+        navigator.clipboard
+          .writeText(shareUrl)
+          .then(() => {
+            window.alert(`Send the following link to anyone you wish to share this configuration with:\n
+${shareUrl}\n
+Shared configuration links expire after 7 days.
+The configuration has already been added to your clipboard`);
+          })
+          .catch(() => console.log('failed to write to clipboard'));
       });
   };
 
