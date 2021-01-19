750b37fff8f0b3b3175081e5fb8fdf738857c266
Alvin Cheng <alvincheng50@gmail.com>
2021-01-18 19:32:49 -0500

    Tweaked how import 'button(s)' works so it(/they) can be styled

--- client/components/ImportBtn.jsx
+++ client/components/ImportBtn.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const ImportBtn = ({ importHandler }) => {
   return (
     <div>
-      <input id="import-btn" type="file" onChange={importHandler} />
+      <input id="import-input" type="file" onChange={importHandler} />
+      <input id="import-btn" className="ui-btn" type="button" value="Import .eslintrc.json" 
+        onClick={() => document.getElementById('import-input').click()}
+      />
     </div>
   );
 }
